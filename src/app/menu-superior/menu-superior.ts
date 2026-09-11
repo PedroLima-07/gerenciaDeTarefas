f35ee96f05986ada529dac5989a05959657a8599
PedroLima-07 <pedrocardosolima20@gmail.com>
2026-09-10 21:25:22 -0300

    upda

---
+++ src/app/menu-superior/menu-superior.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-menu-superior',
+  styleUrl: './menu-superior.css',
+  templateUrl: './menu-superior.html',
+})
+export class MenuSuperior {}
