cedb3314df913e4ec1e2439acd3d4c97bd044008
PedroLima-07 <pedrocardosolima20@gmail.com>
2026-09-10 21:55:03 -0300

    TO perdido!!

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,9 +1,41 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IGerenciador } from '../models/IGerenciador';
 
 @Component({
-  imports: [],
+  imports: [CommonModule, FormsModule],
   selector: 'app-home',
   styleUrl: './home.css',
   templateUrl: './home.html',
 })
-export class Home {}
+export class Home {
+   nome: string = 'Pedro';
+  qtdeClicks: number = 0;
+  mostrarTabela: boolean = true;
+  adicionar: string = '';
+  listaTarefas: IGerenciador[] = [
+    { id: 1, nome: 'Pedro', ativo: true },
+    { id: 2, nome: 'Maria', ativo: true },
+    { id: 3, nome: 'João', ativo: false },
+  ];
+
+  adicionarTarefa() {
+    if (this.adicionar.trim() !== '') {
+      const novaTarefa: IGerenciador = {
+        id: this.listaTarefas.length + 1,
+        nome: this.adicionar,
+        ativo: true,
+      };
+      this.listaTarefas.push(novaTarefa);
+      this.adicionar = '';
+    }
+  }
+
+  removerTarefa(tarefa: IGerenciador) {
+    const index = this.listaTarefas.indexOf(tarefa);
+    if (index > -1) {
+      this.listaTarefas.splice(index, 1);
+    }
+  }
+}
---
+++ src/app/models/IGerenciador.ts
@@ -0,0 +1,5 @@
+export interface IGerenciador{
+    id:number;
+    nome:string;
+    ativo:boolean;
+}
